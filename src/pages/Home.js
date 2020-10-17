82f930893db7f100641918973e9bb6c29701255e
Daniel Nascimento <danielhn@gmail.com>
2020-10-17 13:48:03 -0400

    Starting page

---
+++ src/pages/Home.js
@@ -0,0 +1,26 @@
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import React from "react";
+import ExploreContainer from "../components/ExploreContainer";
+
+const Home = () => {
+  return (
+    <IonPage>
+      <IonContent fullscreen>
+        <IonHeader collapse="condense">
+          <IonToolbar>
+            <IonTitle size="large">Blank</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <ExploreContainer />
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Home;
