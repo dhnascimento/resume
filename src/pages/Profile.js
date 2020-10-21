7e7fa5a44c255597c7fff125878d8cea1f90af23
Daniel Nascimento <danielhn@gmail.com>
2020-10-20 20:12:12 -0400

    Icons in profile component

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -6,8 +6,12 @@ import {
   IonTitle,
   IonThumbnail,
   IonToolbar,
+  IonIcon,
 } from "@ionic/react";
+import { logoLinkedin, logoGithub } from "ionicons/icons";
 import React from "react";
+import { Link } from "react-router-dom";
+import Description from "./DescriptionFlip";
 
 const Profile = () => {
   const display = {
@@ -25,15 +29,34 @@ const Profile = () => {
     "padding-right": "1rem",
   };
 
+  const anchor = {
+    color: "#FFF9C0",
+    display: "flex",
+    "align-items": "flex-end",
+  };
+
+  const anchorDisplay = {
+    display: "flex",
+    "flex-direction": "row-reverse",
+  };
+
   return (
     <div style={display}>
       <IonThumbnail style={{ height: "25%", width: "25%" }}>
-        <IonImg
-          src={"https://i.imgur.com/lq1p6Ll.png?1"}
-          style={{ "border-radius": "" }}
-        />
+        <IonImg src={"https://i.imgur.com/lq1p6Ll.png?1"} />
       </IonThumbnail>
-      <div style={mystyles}>Daniel Nascimento</div>
+      <div style={mystyles}>
+        <span>Daniel Nascimento</span>
+        <div style={anchorDisplay}>
+          <a href="http://linkedin.com/in/danielhnascimento" style={anchor}>
+            <IonIcon icon={logoLinkedin} />
+          </a>
+          <a href="https://github.com/dhnascimento" style={anchor}>
+            <IonIcon icon={logoGithub} />
+          </a>
+        </div>
+      </div>
+
       {/* <p>
         I have over 9 years of experience in the banking and finance industries.
         I’ve always enjoyed working in helping people finding the most suitable
